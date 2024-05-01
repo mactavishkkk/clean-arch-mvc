b692fd472945300773f7caf634c50cfa1f2cfcbc
isaías <isaiasleite230@gmail.com>
2024-05-01 18:25:29 -0300

    adicionando conceitos do DDD

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,9 +1,46 @@
-﻿namespace CleanArchMvc.Domain.Entities
+﻿using CleanArchMvc.Domain.Validation;
+using System.Data;
+
+namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public class Category : Entity
     {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public ICollection<Product> Products { get; set; }
+        public string Name { get; private set; } = string.Empty;
+        public ICollection<Product> Products { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateNameAssign(name);
+        }
+
+        public Category(int id, string name)
+        {
+            ValidateIdAssign(id);
+            ValidateNameAssign(name);
+        }
+
+        public void Update(string name)
+        {
+            ValidateNameAssign(name);
+        }
+
+        private void ValidateIdAssign(int id)
+        {
+            DomainExceptionValidation.When(id <= 0,
+                "Invalid Id, the id must be greater than 0.");
+
+            Id = id;
+        }
+
+        private void ValidateNameAssign(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name, the name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters.");
+
+            Name = name;
+        }
     }
 }
---
+++ CleanArchMvc.Domain/Validation/DomainExceptionValidation.cs
@@ -0,0 +1,15 @@
+﻿namespace CleanArchMvc.Domain.Validation
+{
+    internal class DomainExceptionValidation : Exception
+    {
+        public DomainExceptionValidation(string message) : base(message) { }
+
+        public static void When(bool hasError, string message)
+        {
+            if (hasError)
+            {
+                throw new DomainExceptionValidation(message);
+            }
+        }
+    }
+}
