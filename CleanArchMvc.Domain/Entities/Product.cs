2331c0a2aaa0aa9c50095436fe58e287938ca512
isaías <isaiasleite230@gmail.com>
2024-05-03 16:46:56 -0300

    NullException (Product)

--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -58,7 +58,7 @@ namespace CleanArchMvc.Domain.Entities
             DomainExceptionValidation.When(stock < 0,
                "Invalid stock, the stock must be positive.");
 
-            DomainExceptionValidation.When(image.Length > 250,
+            DomainExceptionValidation.When(image?.Length > 250,
                "Invalid image name, too long, maximum 250 characters.");
 
             Name = name;
