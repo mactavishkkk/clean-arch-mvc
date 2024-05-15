2c80d090d1a88e9ebe0c8daf8b8c10661adea6cd
isaías <isaiasleite230@gmail.com>
2024-05-15 20:31:16 -0300

    home controller + create category (post)

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -17,6 +18,23 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             var categories = await _categoryService.GetAllAsync();
             return View(categories);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDTO categoryDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.CreateAsync(categoryDTO);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoryDTO);
         }
     }
 }
